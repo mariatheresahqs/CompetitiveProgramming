e7269a9b12f18661b8860a3e021891e39078a27f
Maria Theresa <mariatheresahenriques@gmail.com>
2018-11-26 07:12:36 -0200

    Create uri1099.py

---
+++ Codes/uri1099.py
@@ -0,0 +1,8 @@
+for i in range(int(input(""))):
+    l, s = sorted([int(x) for x in input("").split()]), 0
+
+    for i in range(l[0] + 1, l[1]):
+        if i%2 != 0:
+            s += i
+
+    print(s)
