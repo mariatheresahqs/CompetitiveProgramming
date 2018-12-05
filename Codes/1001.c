3cc76be7c1425d39ae5ce29f582fe13c4806ae55
Maria Theresa <mariatheresahenriques@gmail.com>
2018-12-05 16:59:11 -0200

    Create 1001.c

---
+++ Codes/1001.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+    int z;
+    int X;
+
+    scanf("%d", &x);
+    scanf("%d", &z);
+
+    X = x+z;
+
+    printf("X = %d\n", X);
+
+  return 0;
+}
