257ec080276eb60a62c79bbe34fb569016aff7b0
Maria Theresa <mariatheresahenriques@gmail.com>
2018-12-06 21:18:19 -0200

    Create uri1004.c

---
+++ Codes/uri1004.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int x;
+    int z;
+    int PROD;
+
+    scanf("%d %d",&x, &z);
+
+    PROD= x*z;
+
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
