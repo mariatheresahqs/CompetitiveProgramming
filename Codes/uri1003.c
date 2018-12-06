e2865463b5d9460507f71ea455e8a3c56b5acc67
Maria Theresa <mariatheresahenriques@gmail.com>
2018-12-06 21:17:58 -0200

    Create uri1003.c

---
+++ Codes/uri1003.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int x;
+    int z;
+    int sum;
+
+    scanf("%d %d", &x, &z);
+
+    sum= x + z;
+
+    printf("SOMA = %d\n", sum);
+
+    return 0;
+
+}
