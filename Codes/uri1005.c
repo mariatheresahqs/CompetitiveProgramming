8d34c6d7b7be4232fca7f29480d3d3f94c6e79db
Maria Theresa <mariatheresahenriques@gmail.com>
2018-12-06 21:18:40 -0200

    Create uri1005.c

---
+++ Codes/uri1005.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double  A, B, MEDIA;
+
+    scanf("%lf %lf", &A, &B);
+
+    MEDIA= (A*3.5 + B*7.5)/11;
+
+    printf("MEDIA = %.5lf\n", MEDIA);
+
+    return 0;
+
+}
