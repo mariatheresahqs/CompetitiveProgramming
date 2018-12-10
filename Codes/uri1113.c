10c4f35aec921b4af5e488d2e952e62c9258c211
Maria Theresa <mariatheresahenriques@gmail.com>
2018-12-10 10:41:58 -0200

    Create uri1113.c

---
+++ Codes/uri1113.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int y,x;
+    while(1){
+        scanf("%d %d",&x, &y);
+        if (x==y)
+            break;
+        else if (x>y)
+            printf("Decrescente\n");
+        else
+            printf("Crescente\n");
+        }
+    return 0;
+}
