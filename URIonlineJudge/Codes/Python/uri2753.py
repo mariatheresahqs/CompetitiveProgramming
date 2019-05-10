e134d1e55f56f4687555fd4c32f0f246a4ba5dc6
Maria Theresa <mariatheresahenriques@gmail.com>
2019-05-09 23:28:24 -0300

    uri2753

---
+++ URIonlineJudge/Codes/Python/uri2753.py
@@ -0,0 +1,3 @@
+alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z"]
+for i in range(0, 26):
+    print("{} e {}".format(i+97, alfabeto[i]))
