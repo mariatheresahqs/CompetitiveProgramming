c0c20d57bf0c0f6cbd862f663a9777fbab584801
Maria Theresa <mariatheresahenriques@gmail.com>
2019-05-11 12:55:20 -0300

    uri1115

---
+++ URIonlineJudge/Codes/Python/uri1115.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        x, y = map(int, input().split())
+        if (x > 0 and y > 0):
+            print("primeiro")
+        elif (x < 0 and y > 0):
+            print("segundo")
+        elif (x < 0 and y < 0):
+            print("terceiro")
+        elif (x > 0 and y < 0):
+            print("quarto")
+        elif (x == 0 or y == 0):
+            break
+
+    except EOFError:
+        break
