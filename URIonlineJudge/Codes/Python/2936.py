9ed2d34f58be6370679fbd5eedb621120ec14a48
Isabella Ramos <isabellamenezesramos@outlook.com>
2020-10-19 14:18:30 -0300

    Create 2936.py

---
+++ URIonlineJudge/Codes/Python/2936.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+curupira = 300 * a
+boitata = 1500 * b
+boto = 600 * c
+mapinguari = 1000 * d
+iara = 150 * e
+
+print(curupira + boitata + boto + mapinguari + iara + 225)
